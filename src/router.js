144b6ac3d40c8e3ba56bb424b90ea84c0b6b6bc7
nonowelle <menoel888@gmail.com>
2021-09-25 13:35:53 -0400

    adding confirmation path

--- src/router.js
+++ src/router.js
@@ -3,18 +3,20 @@ import VueRouter from "vue-router";
 
 // import App from "./App.vue";
 
-// import ConfirmationsLists from "./components/pages/ConfirmationsList.vue";
+import ConfirmationsList from "./components/pages/ConfirmationsList.vue";
 // import NotFound from "./components/pages/NotFound.vue";
 import Home from "./components/Home.vue";
+// import Mariage from "./components/Mariage.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
     { path: "/", component: Home },
-    // { path: "/confirmations", component: ConfirmationsLists },
+    { path: "/confirmations", component: ConfirmationsList },
     // { path: "/:notFound(.*)", component: NotFound },
   ],
+  mode: "history",
 });
 
 export default router;
