319bac8c7104145bd92cf33b05bc8aab403e95b5
nonowelle <menoel888@gmail.com>
2021-10-10 11:47:15 -0400

    build and change start script

--- app.js
+++ app.js
@@ -8,15 +8,10 @@ const bodyParser = require("body-parser");
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use("/static", express.static(__dirname + "/../build"));
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
-
-  // Request methods you wish to allow
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  // );
 
   // Request headers you wish to allow
   res.setHeader(
