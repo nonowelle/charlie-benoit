3f8bfab2f9badad860da1297126dc9c0029fa2ed
nonowelle <menoel888@gmail.com>
2021-10-11 20:34:04 -0400

    serve dist folder

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use("/", serveStatic(path.join(__dirname + "/dist")));
+app.use("/", serveStatic(path.join(__dirname + "/dist")));
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
